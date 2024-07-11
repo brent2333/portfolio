bc7ce08f130e7aa40823273d414475cec5444238
Brent Lawson <brent.lawson@fleetcor.com>
2024-07-11 09:32:04 -0400

    fetch remaining posts

--- public/routes/posts.js
+++ public/routes/posts.js
@@ -5,7 +5,17 @@ const pool = require("../../db");
 const postsRouter = express.Router();
 
 postsRouter.get("/remainder/:id", (request, response) => {
-  response.status(200).send("test");
+  pool.query(
+    `SELECT * FROM posts WHERE post_id = ${request.params.id}`,
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      if (results.rows && results.rows.length) {
+        response.status(200).json(results.rows[0]);
+      }
+    }
+  );
 });
 
 module.exports = postsRouter;
