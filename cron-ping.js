a4196c3a3beaa897faf1df3658e50c1539fac6c4
Brent Lawson <brent.lawson@fleetcor.com>
2024-07-15 17:23:30 -0400

    db stuff

--- cron-ping.js
+++ cron-ping.js
@@ -1,7 +1,7 @@
 const { fetchGuitars } = require("./utils/amazon");
 const cron = require("node-cron");
 
-cron.schedule("* * 23 * *", function () {
+cron.schedule("* * * * *", function () {
   console.log("running task every 5 minutes");
   fetchGuitars();
 });
